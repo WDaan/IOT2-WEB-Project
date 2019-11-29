b2c64e50430fd188a0a88ab85f45ebfb12a306fd
wdaan <dw.daanwijns@gmail.com>
2019-11-29 14:33:37 +0000

    changet to ajax call for project requirements

--- js/main.js
+++ js/main.js
@@ -5,64 +5,67 @@ $(document).ready(function () {
 })
 
 function handleFunction(fun, params, id) {
-  new Promise((res, rej) => {
-    params ? fun(params) : fun();
-    res();
-  }).finally(() => {
-    document.getElementById(id).remove();
-  });
+    new Promise((res, rej) => {
+        params ? fun(params) : fun();
+        res();
+    }).finally(() => {
+        document.getElementById(id).remove();
+    });
 }
 
 function drawTable(data, prepend) {
-  data.forEach(el => {
-    //rij
-    let trow = document.createElement("tr");
-    trow.classList.add("trow");
-    trow.id = el.id;
-    //data cols
-    let tijd = document.createElement("th");
-    let expected = document.createElement("td");
-    let word = document.createElement("td");
+    data.forEach(el => {
+        //rij
+        let trow = document.createElement("tr");
+        trow.classList.add("trow");
+        trow.id = el.id;
+        //data cols
+        let tijd = document.createElement("th");
+        let expected = document.createElement("td");
+        let word = document.createElement("td");
 
-    //data toevoegen
-    tijd.innerHTML = el.time;
-    expected.innerHTML = el.expected;
-    word.innerHTML = el.word;
+        //data toevoegen
+        tijd.innerHTML = el.time;
+        expected.innerHTML = el.expected;
+        word.innerHTML = el.word;
 
-    //styling
-   expected.setAttribute("align", "center");
-   word.setAttribute("align", "center");
+        //styling
+        expected.setAttribute("align", "center");
+        word.setAttribute("align", "center");
 
-    //adding to table
-    trow.appendChild(tijd);
-    trow.appendChild(expected);
-    trow.appendChild(word);
+        //adding to table
+        trow.appendChild(tijd);
+        trow.appendChild(expected);
+        trow.appendChild(word);
 
-    prepend ? tbody.prepend(trow) : tbody.append(trow);
-  });
+        prepend ? tbody.prepend(trow) : tbody.append(trow);
+    });
 
-  if (localStorage.getItem('history')) {
-    let ids = JSON.parse(localStorage.getItem('history'));
-    if (ids.length >= 30) {
-      let remove = ids.splice(data.length);
-      console.log(remove);
-      remove.forEach(el => document.getElementById(el).remove());
-      //enkel oveblijvende ids
-      localStorage.setItem('history', JSON.stringify(ids));
+    if (localStorage.getItem('history')) {
+        let ids = JSON.parse(localStorage.getItem('history'));
+        if (ids.length >= 30) {
+            let remove = ids.splice(data.length);
+            console.log(remove);
+            remove.forEach(el => document.getElementById(el).remove());
+            //enkel oveblijvende ids
+            localStorage.setItem('history', JSON.stringify(ids));
+        }
     }
-  }
-  else
-    localStorage.setItem('history', JSON.stringify(data.map(el => el.id)));
+    else
+        localStorage.setItem('history', JSON.stringify(data.map(el => el.id)));
 
 }
 
 
 function setLeds(color) {
-    // console.log(color);
     const format = str_pad(color.b, 3) + str_pad(color.g, 3) + str_pad(color.r, 3)
-    axios.get(`http://pi4:3000/color?color=${format}`).then(res => {
-    })
-
+    $.ajax({
+        type: "GET",
+        url: "http://pi4:3000/color?color=${format}`",
+        error: function () {
+            console.error("Couldn't set color");
+        }
+    });
     document.getElementById("color").innerHTML = `bgr(${color.b},${color.g},${color.r})`
 }
 
